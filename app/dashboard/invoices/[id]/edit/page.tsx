5d4592aa237ede50a66e9d20c56f3c563e0b2ccf
kanterbury <ksuzuki.db@gmail.com>
2024-01-12 10:52:01 +0900

    編集ページのメタデータの静的設定

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,21 +2,11 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
-import { Metadata, ResolvingMetadata } from "next";
+import { Metadata } from "next";
 
-export async function generateMetaData(
-  {
-    params
-  }: {
-    params: { id: string };
-  },
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const { id } = params;
-  return {
-    title: `Edit Invoice #${id}`
-  };
-}
+export const metadata: Metadata = {
+  title: "Edit Invoice"
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
