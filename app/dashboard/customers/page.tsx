6cb6a5a2bd725f2e3dc3b798780d630cb4f95966
kanterbury <ksuzuki.db@gmail.com>
2024-01-23 20:16:13 +0900

    顧客一覧画面の作成

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,26 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+import Table from "@/app/ui/customers/table";
+
 export const metadata = {
   title: "Customers"
 };
 
-export default function Page() {
+export default async function Page({
+  searchParams
+}: {
+  searchParams: { query?: string };
+}) {
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
   return (
-    <>
-      <p>Customers Page</p>
-    </>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <Suspense fallback={<div>Loading... </div>}>
+          <Table customers={customers} />
+        </Suspense>
+      </div>
+    </div>
   );
 }
